f1b8f7c6e14e3714c2dddefd3bd0716e54ee1f87
Leal31 <lealemanuel31@protonmail.com>
2022-03-28 16:09:41 -0500

    calculadora

--- calculadora/main.js
+++ calculadora/main.js
@@ -20,45 +20,23 @@ var resta = document.getElementById("resta");
 var multiplicacion = document.getElementById("multiplicacion");
 var division = document.getElementById("division");
 var display = document.getElementById("display");
-var operador = "";
-var num1 = "";
-var num2 = "";
 var resultado = document.getElementById("resultado");
 var acumulado=0;
-
+var sumar = false;
+var restar = false;
+var multiplicar = false;
+var dividir = false;
+// Por si no hemos hecho operaciones previas
+var poperacion = true;
+var borrar = document.getElementById("borrar"); // variable que borrara operacion alguna vez hecha;
 // Metodo que escribira numeros que se opriman
 function escribirNumero(numero) {
   display.value = cifra + numero;
   cifra = display.value;
 
 }
-
-function operar(operador) {
-  switch (operador) {
-    case "+":
-      acumulado = acumulado + parseInt(cifra);
-      display.value = acumulado;
-      cifra = "";
-      break;
-    case "-":
-      acumulado = acumulado - parseInt(cifra);
-      display.value = acumulado;
-      cifra="";
-      break;
-    case "*":
-      acumulado = acumulado * parseInt(cifra);
-      display.value = acumulado;
-      cifra = "";
-      break;
-    case "/":
-      acumulado = acumulado / parseInt(cifra);
-      display.value = acumulado;
-      cifra = "";
-    default:
-
-  }
-}
-
+// Funciones de teclas las cuales funcionaran en base al numero que representen
+// La idea es que al presionar los botones se escriban en la variable "display"
 uno.onclick = function(e) {
   escribirNumero(1);
 }
@@ -99,26 +77,128 @@ cero.onclick = function(e) {
   escribirNumero(0);
 }
 
+borrar.onclick = function(e) {
+  display.value = 0;
+  acumulado = 0;
+  cifra = "";
+}
+// A la hora de darle al simbolo "sumar"
 suma.onclick = function (e) {
-  operador = "+";
-  operar(operador);
+    // verifica si se ha hecho una operación previa
+    if (restar) {
+      acumulado = acumulado - parseInt(cifra);
+      display.value = acumulado;
+    }  else if (multiplicar){
+      acumulado = acumulado * parseInt(cifra);
+      display.value = acumulado;
+    } else if (dividir) {
+      acumulado = acumulado / parseInt(cifra);
+      display.value = acumulado;
+    // Si no se ha hecho una operación previa se hara la operación que concuerde con el simbolo
+    // se aplicara a las siguientes funciones parecidas
+    } else {
+      acumulado = acumulado + parseInt(cifra);
+      display.value = acumulado;
+    }
+
+  cifra = "";
+  restar = false;
+  multiplicar = false;
+  dividir = false
+  sumar = true;
+  poperacion = false;
 }
 
 resta.onclick = function (e) {
-  operador = "-";
-  operar(operador);
+  // Si se ha hecho operacion previa se haran lo mismo que en la funcion suma()
+  if (poperacion == false) {
+    if (sumar) {
+      acumulado = acumulado + parseInt(cifra);
+      display.value = acumulado;
+    } else if (multiplicar){
+      acumulado = acumulado * parseInt(cifra);
+      display.value = acumulado;
+    } else if (dividir) {
+      acumulado = acumulado / parseInt(cifra);
+      display.value = acumulado;
+    } else {
+      acumulado = acumulado - parseInt(cifra);
+      display.value = acumulado;
+    }
+  // Si es la primera operacion se va a cambiar el acumulado = 0 por lo que este en el display
+  } else {
+    acumulado = parseInt(cifra);
+    poperacion = false; // Se desactiva variable poperacion
+  }
+  cifra = "";
+  sumar = false;
+  restar = true;
+  multiplicar = false;
+  dividir = false;
 }
 
 multiplicacion.onclick = function (e) {
-  operador = "*";
-  operar(operador);
+  if (poperacion == false) {
+    if (sumar) {
+      acumulado = acumulado + parseInt(cifra);
+      display.value = acumulado;
+    } else if (restar){
+      acumulado = acumulado - parseInt(cifra);
+      display.value = acumulado;
+    } else if (dividir) {
+      acumulado = acumulado / parseInt(cifra);
+      display.value = acumulado;
+    } else {
+      acumulado = acumulado * parseInt(cifra);
+      display.value = acumulado;
+    }
+  } else {
+    acumulado = parseInt(cifra);
+    poperacion = false;
+  }
+  cifra = "";
+  sumar = false;
+  restar = false;
+  multiplicar = true;
+  dividir = false;
 }
 
 division.onclick = function (e) {
-  operador = "/";
-  operar(operador);
+  if (poperacion == false) {
+    if (sumar) {
+      acumulado = acumulado + parseInt(cifra);
+      display.value = acumulado;
+    } else if (restar){
+      acumulado = acumulado - parseInt(cifra);
+      display.value = acumulado;
+    } else if (multiplicar) {
+      acumulado = acumulado * parseInt(cifra);
+      display.value = acumulado;
+    } else {
+      acumulado = acumulado / parseInt(cifra);
+      display.value = acumulado;
+    }
+  } else {
+    acumulado = parseInt(cifra);
+    poperacion = false;
+  }
+  cifra = "";
+  sumar = false;
+  restar = false;
+  multiplicar = false;
+  dividir = true;
 }
 
 resultado.onclick = function(e) {
-  operar(operador);
+  if (sumar) {
+    display.value = acumulado + parseInt(cifra);
+  } else if (restar) {
+    display.value = acumulado - parseInt(cifra);
+  } else if (multiplicar) {
+    display.value = acumulado * parseInt(cifra);
+  } else if (dividir) {
+    display.value = acumulado / parseInt(cifra);
+  }
+  acumulado = parseInt(display.value);
+  cifra = 0;
 }
