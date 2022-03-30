845d8a24c7a91ba5c0d01772873d3a185e87fc5f
Leal31 <lealemanuel31@protonmail.com>
2022-03-30 16:34:31 -0500

    jquery

---
+++ jQuery/prueba.js
@@ -0,0 +1,11 @@
+/*
+var mensajes = document.querySelectorAll("#carrete h1");
+
+for (var i = 0;i < mensajes.length;i++) {
+  mensajes[i].style.visibility = "hidden";
+}
+*/
+
+$(document).ready(function(e) {
+  $ ("#carrete h1").hide();
+});
